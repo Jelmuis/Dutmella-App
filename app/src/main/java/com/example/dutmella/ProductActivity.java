b98a2022710e127ab680a94c56d6ad4c1a05a8e8
Jeralta <47526765+Jeralta@users.noreply.github.com>
2019-02-26 15:02:40 +0100

    Revert "Development"

--- app/src/main/java/com/example/dutmella/ProductActivity.java
+++ app/src/main/java/com/example/dutmella/ProductActivity.java
@@ -9,6 +9,5 @@ public class ProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
-        String test;
     }
 }
